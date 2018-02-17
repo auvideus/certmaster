512e0a2f73808d99c2b15b31e0cc85d3c79ba49b
auvideus <eric.huneke@protonmail.com>
2018-02-17 16:27:16 -0600

    refactor testing util to own file

--- certbot_test.go
+++ certbot_test.go
@@ -2,23 +2,7 @@ package certmaster
 
 import (
 	"testing"
-	"os/exec"
 )
-
-// mockCmd mocks an exec.Cmd object by storing one as an anonymous field.
-type mockCmd struct {
-	*exec.Cmd
-}
-
-// CombinedOutput allows the mockCmd type to implement CmdI.
-func (c *mockCmd) CombinedOutput() ([]byte, error) {
-	return []byte(c.Path + " executed"), nil
-}
-
-// mockCmdBuilder mocks getting a Cmd object.
-func mockCmdBuilder(name string, arg ...string) cmdI {
-	return &mockCmd{exec.Command(name, arg...)}
-}
 
 // TestCallCertbot verifies that the configuration is set appropriately.
 func TestCallCertbot(t *testing.T) {
