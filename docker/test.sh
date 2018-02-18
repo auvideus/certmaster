ad3c8e0874dbfd988d31eb9c63e4bf04e05beda5
auvideus <eric.huneke@protonmail.com>
2018-02-18 12:50:46 -0600

    docker test script for CI

--- docker/test.sh
+++ docker/test.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-docker build -t certmaster-server -f docker/Dockerfile_server
-docker build -t certmaster-server-test -f docker/test/Dockerfile_server
-docker run -t certmaster-server
-docker build -t certmaster-client -f docker/Dockerfile_client
-docker build -t certmastesr-client-test -f docker/test/Dockerfile_client
-docker run -t certmaster-client
+echo -e "\n-----> BUILDING SERVER BASE\n"
+docker build -t certmaster-server -f docker/Dockerfile_server .
+echo -e "\n-----> BUILDING SERVER TEST\n"
+docker build -t certmaster-server-test -f docker/test/Dockerfile_server .
+echo -e "\n-----> RUNNING SERVER TEST\n"
+docker run -t certmaster-server-test
+echo -e "\n-----> BUILDING CLIENT BASE\n"
+docker build -t certmaster-client -f docker/Dockerfile_client .
+echo -e "\n-----> BUILDING CLIENT TEST\n"
+docker build -t certmaster-client-test -f docker/test/Dockerfile_client .
+echo -e "\n-----> RUNNING CLIENT TEST\n"
+docker run -t certmaster-client-test
