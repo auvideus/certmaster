64c94095f06c2dc8f31794b78b77381926ec4fb6
auvideus <eric.huneke@protonmail.com>
2018-02-19 12:40:36 -0600

    move login and push to build script

--- docker/build.sh
+++ docker/build.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+export DOCKER_USERNAME=auvideus
 echo -e "\n-----> BUILDING SERVER BASE\n"
-docker build -t certmaster-server -f docker/Dockerfile_server .
+docker build -t $DOCKER_USERNAME/certmaster-server -f docker/Dockerfile_server .
 echo -e "\n-----> BUILDING SERVER TEST\n"
 docker build -t certmaster-server-test -f docker/test/Dockerfile_server .
 echo -e "\n-----> RUNNING SERVER TEST\n"
 docker run -t certmaster-server-test
 echo -e "\n-----> BUILDING CLIENT BASE\n"
-docker build -t certmaster-client -f docker/Dockerfile_client .
+docker build -t $DOCKER_USERNAME/certmaster-client -f docker/Dockerfile_client .
 echo -e "\n-----> BUILDING CLIENT TEST\n"
 docker build -t certmaster-client-test -f docker/test/Dockerfile_client .
 echo -e "\n-----> RUNNING CLIENT TEST\n"
 docker run -t certmaster-client-test
+
+if [[ -v DOCKER_PASSWORD ]]; then
+    docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+    docker push "$DOCKER_USERNAME"/certmaster-server
+    docker push "$DOCKER_USERNAME"/certmaster-client
+fi
